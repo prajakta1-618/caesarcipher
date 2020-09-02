0e3a8b0effd9195bd4801548d869634a5f2b3663
prajakta1-618 <66864802+prajakta1-618@users.noreply.github.com>
2020-09-02 22:45:02 +0530

    Delete GetKey.py

--- GetKey.py
+++
@@ -1,3 +0,0 @@
-alice = int(input('Enter public key shared by Alice:'))
-secretkey = (alice ^ secret) % mod
-print(f'The secret key shared by you and Alice is {secretkey}. Do not share this with anyone. Enter this in the cipher "encryption.py" or "Decryption.py"')
