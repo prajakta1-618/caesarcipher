26cdd3408d167499df0cd9cf7fc479f28a59a05c
prajakta1-618 <66864802+prajakta1-618@users.noreply.github.com>
2020-09-02 22:43:51 +0530

    Update sharingkeys.py

--- sharingkeys.py
+++ sharingkeys.py
@@ -1,8 +1,8 @@
 mod = int(input('Enter the publicly decided number to find the mod to: '))
-base = int(input('Enter the publically decided base:'))
+base = int(input('Enter the publically decided generator:'))
 secret = int(input('Enter the secret number Bob chose as the index:'))
 
-bob = (base ^ secret) % mod
+bob = (base ** secret) % mod
 
 print('Your publicaly sharable key is:', bob)
 print('Share this with Alice.')
@@ -10,7 +10,7 @@ print('Share this with Alice.')
 que = input(' Do you have public key from Alice?(y/n)')
 if que == 'y':
     alice = int(input('Enter public key shared by Alice:'))
-    secretkey = (alice ^ secret) % mod
+    secretkey = (alice ** secret) % mod
     print(f'The secret key shared by you and Alice is {secretkey}. Do not share this with anyone.')
 
 elif que == 'n':
